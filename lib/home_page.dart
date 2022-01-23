38e626d3c153d737fd1ae97a0b0fdbfe69ed915e
Bimal Yadav <bimalyadavemail7@gmail.com>
2022-01-23 16:41:35 +0545

    day 2

---
+++ lib/home_page.dart
@@ -0,0 +1,21 @@
+// import material package
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      drawer: Drawer(),
+      appBar: AppBar(
+        title: Text('Catalog App'),
+      ),
+      body: Center(
+        child: Container(
+          child: Text('Home Page'),
+        ),
+      ),
+    );
+  }
+}
